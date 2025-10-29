cfd1800a13ea96a780c99afb6a33fb3669bab71f
ritesh <riteshpatidar088@gmail.com>
2025-10-29 09:46:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { View, Text, StyleSheet, Image, Button, Alert } from 'react-native';
+// function App() {
 
-export default function App() {
+//   const handlePress = () => {
+//     Alert.alert("THIS IS ALERT")
+//   }
+//   return (
+//     <View style={styles.container}>
+//       <View style={{ backgroundColor: 'blue', height: 100, width: 150 }}>
+//         <Text style={styles.bold}>BOX - 1</Text>
+//       </View>
+//       <View style={{ backgroundColor: 'red', height: 80, width: 150 }}>
+//         <Text style={styles.bold}>BOX 2</Text>
+//       </View>
+
+//       <Image
+//         style={styles.reactImg}
+//         source={{
+//           uri: 'https://tse1.explicit.bing.net/th/id/OIP.-v7hmnipjMTO8z4AyzqcpAHaHa?pid=ImgDet&w=195&h=195&c=7&dpr=1.4&o=7&rm=3',
+//         }}
+//       />
+//       <Image
+//         style={styles.reactImg}
+//         source={require('./assets/splash-icon.png')}
+//       />
+//       <Button title='CLICK' onPress={handlePress}/>
+
+//     </View>
+//   );
+// }
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   bold: {
+//     fontSize: 50,
+//     color: 'white',
+//     fontWeight: 800,
+//   },
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   reactImg: {
+//     height: 100,
+//     width: 100,
+//   },
+// });
+
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  console.log(data);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>App</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
