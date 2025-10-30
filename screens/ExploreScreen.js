3ab09a648c16631fa6de9e7000014ff37eba0daa
ritesh <riteshpatidar088@gmail.com>
2025-10-30 10:31:48 +0530

    updated till the nested routing

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 const ExploreScreen = () => {
+    const navigation = useNavigation()
+    const onPress = () => {
+navigation.navigate('Home')
+    }
   return (
     <View>
-      <Text>ExploreScreen</Text>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text>Go to home screen</Text>
+        </TouchableOpacity>
     </View>
   )
 }
 
 export default ExploreScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+      },
+})
